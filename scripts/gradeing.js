b774aa25ee20b8c391e2d8dbdabb815c199ec309
Tahira Jannat <jannat.setu95@gmail.com>
2024-01-02 12:03:29 +0600

    Conditions and Switch

--- scripts/gradeing.js
+++ scripts/gradeing.js
@@ -1,14 +1,14 @@
 /* Program for Task 4*/
 // Letter Grade Program. Happy coding
 
-// var marks = parseInt(prompt('Enter Marks : '));
+var marks = parseInt(prompt('Enter Marks : '));
 
-// // checking the conditions
-// if (marks > 100 || marks < 0) document.write('Invalid marks');
-// else if (marks >= 80 && marks <= 100) document.write('A+');
-// else if (marks >= 70 && marks < 80) document.write('A');
-// else if (marks >= 60 && marks < 70) document.write('A-');
-// else if (marks >= 50 && marks < 60) document.write('B');
-// else if (marks >= 40 && marks < 50) document.write('C');
-// else if (marks >= 33 && marks < 40) document.write('D');
-// else document.write('Sorry. You are Failed');
+// checking the conditions
+if (marks > 100 || marks < 0) document.write('Invalid marks');
+else if (marks >= 80 && marks <= 100) document.write('A+');
+else if (marks >= 70 && marks < 80) document.write('A');
+else if (marks >= 60 && marks < 70) document.write('A-');
+else if (marks >= 50 && marks < 60) document.write('B');
+else if (marks >= 40 && marks < 50) document.write('C');
+else if (marks >= 33 && marks < 40) document.write('D');
+else document.write('Sorry. You are Failed');
--- scripts/largeNumber.js
+++ scripts/largeNumber.js
@@ -1,26 +1,26 @@
-// var num1 = prompt('Enter num! : ');
-// var num2 = prompt('Enter num2 : ');
-// var num3 = prompt('Enter num3 : ');
+var num1 = prompt('Enter num! : ');
+var num2 = prompt('Enter num2 : ');
+var num3 = prompt('Enter num3 : ');
 
-// if (num1 > num2 && num1 < num3) {
-//     console.log('Large Number=' + num1);
-// } else if (num2 > num1 && num2 < num3) {
-//     console.log('Large Number=' + num2);
-// } else {
-//     console.log('Large Number=' + num3);
-// }
+if (num1 > num2 && num1 < num3) {
+    console.log('Large Number=' + num1);
+} else if (num2 > num1 && num2 < num3) {
+    console.log('Large Number=' + num2);
+} else {
+    console.log('Large Number=' + num3);
+}
 
-// //Vowel Or consonant
-// var letter = prompt('Enter a letter : ');
-// letter = letter.toLowerCase();
-// if (
-//     letter == 'a' ||
-//     letter == 'e' ||
-//     letter == 'i' ||
-//     letter == 'o' ||
-//     letter == 'u'
-// ) {
-//     console.log('Vowel');
-// } else {
-//     console.log('Consonant');
-// }
+//Vowel Or consonant
+var letter = prompt('Enter a letter : ');
+letter = letter.toLowerCase();
+if (
+    letter == 'a' ||
+    letter == 'e' ||
+    letter == 'i' ||
+    letter == 'o' ||
+    letter == 'u'
+) {
+    console.log('Vowel');
+} else {
+    console.log('Consonant');
+}
