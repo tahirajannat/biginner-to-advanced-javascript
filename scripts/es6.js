6ee2765db041e61201b0a666b2c3ce3b5f84751a
Tahira Jannat <tahirajannat@Tahiras-MacBook-Pro.local>
2024-01-14 23:29:43 +0600

    es6

---
+++ scripts/es6.js
@@ -0,0 +1,60 @@
+// let x = 11;
+// if (true) {
+//     x = 10;
+// }
+// console.log('let', x);
+
+// // const y = 11;
+// // if (true) {
+// //     y = 10;
+// // }
+// // console.log('const', y);
+
+// let a = 20;
+// let b = 30;
+// var sum = a + b;
+
+// let name = 'Tahira Jannat';
+// let message = `I am ${name}. I am learning javascript`;
+// console.log(message);
+
+// ///ES6 function
+
+// // function add(x, y) {
+// //     var sum = x + y;
+// //     console.log(sum);
+// // }
+// // add(10, 10);
+// const add = (x, y) => {
+//     let sum = x + y;
+//     console.log(sum);
+// };
+// add(10, 10);
+
+/**
+ * ES6 features
+ *   1. ES6 syntax
+ *     1.1. ES6 Variables -> var, let, const (scope, redeclaration, value assigning)
+ *     1.2. Template Literals,
+ *     1.3. Hoisting and strict mode
+ *     1.4. Default and Rest parameter
+ *     1.5. spread operator
+ *     1.6. for ...of
+ *
+ *  2. Arrow functions
+ *  3. Destructuring
+ *     3.1. Array and Object Destructuring
+ *  .
+ *  .
+ *  .
+ *
+ */
+
+x = 20;
+if (true) {
+    y = 10;
+    console.log(`y = ${y}`);
+    var y;
+}
+console.log(`x = ${x}`);
+var x;
