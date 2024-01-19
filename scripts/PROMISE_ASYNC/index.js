8d2279066dac42fc156492889a8fa061ae55b68e
Tahira Jannat <jannat.setu95@gmail.com>
2024-01-19 18:36:49 +0600

    Promise async

--- scripts/PROMISE_ASYNC/index.js
+++ scripts/PROMISE_ASYNC/index.js
@@ -4,24 +4,24 @@
 // console.log('task 3');
 // console.log('task 4');
 // console.log('task 5');
-const task1 = () => {
-    console.log('task 1');
-};
-const dataLoading = () => {
-    console.log('task 2 . Data Loading');
-};
-const task2 = () => {
-    setTimeout(dataLoading, 1000);
-};
-const task3 = () => {
-    console.log('task 3');
-};
-const task4 = () => {
-    console.log('task 4');
-};
-task1();
-task2();
-task3();
-task4();
+// const task1 = () => {
+//     console.log('task 1');
+// };
+// const dataLoading = () => {
+//     console.log('task 2 . Data Loading');
+// };
+// const task2 = () => {
+//     setTimeout(dataLoading, 1000);
+// };
+// const task3 = () => {
+//     console.log('task 3');
+// };
+// const task4 = () => {
+//     console.log('task 4');
+// };
+// task1();
+// task2();
+// task3();
+// task4();
 
 //Asynchronous programming
