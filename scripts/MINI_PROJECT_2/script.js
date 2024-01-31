420b1532a2d2552cbff829316e79483826901eb8
Tahira Jannat <jannat.setu95@gmail.com>
2024-01-31 16:17:56 +0600

    Mini project2

--- scripts/MINI_PROJECT_2/script.js
+++ scripts/MINI_PROJECT_2/script.js
@@ -59,3 +59,42 @@ const loadAllData = async () => {
     });
 };
 loadAllData();
+
+let enterEventCount = 0;
+let leaveEventCount = 0;
+const mouseTarget = document.getElementById('mouseTarget');
+const unorderedList = document.getElementById('unorderedList');
+
+mouseTarget.addEventListener('mouseenter', (e) => {
+    mouseTarget.style.border = '5px dotted orange';
+    enterEventCount++;
+    addListItem(`This is mouseenter event ${enterEventCount}.`);
+});
+
+mouseTarget.addEventListener('mouseleave', (e) => {
+    mouseTarget.style.border = '1px solid #333';
+    leaveEventCount++;
+    addListItem(`This is mouseleave event ${leaveEventCount}.`);
+});
+
+function addListItem(text) {
+    // Create a new text node using the supplied text
+    const newTextNode = document.createTextNode(text);
+
+    // Create a new li element
+    const newListItem = document.createElement('li');
+
+    // Add the text node to the li element
+    newListItem.appendChild(newTextNode);
+
+    // Add the newly created list item to list
+    unorderedList.appendChild(newListItem);
+}
+////
+function unhighlight(x) {
+    x.style.backgroundColor = 'transparent';
+}
+
+function highlight(x) {
+    x.style.backgroundColor = 'red';
+}
